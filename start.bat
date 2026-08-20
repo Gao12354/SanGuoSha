5acce63fbe91e74072b50c7603defe2334f368e6
高天一 <giyiyi666@outlook.com>
2026-08-20 22:20:59 +0800

    焕然一新的启动

---
+++ start.bat
@@ -0,0 +1,10 @@
+@echo off
+chcp 65001
+cd /d "%~dp0"
+powershell.exe -Command "Get-ChildItem -Path '.\' -Filter '*.ps1' | Unblock-File"
+fltmc >nul 2>&1 || (
+    echo 正在请求管理员权限...
+    powershell -Command "Start-Process cmd -ArgumentList '/c ""%~f0""' -Verb RunAs"
+    exit
+)
+powershell.exe -ExecutionPolicy Bypass -File ".\start.ps1"
